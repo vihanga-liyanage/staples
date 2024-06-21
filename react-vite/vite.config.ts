35e964ef24526261df7a7d8d581cc826348164f1
Vihanga Liyanage <vihangaliyanage007@gmail.com>
2024-06-21 15:24:22 -0500

    Convert to use npm instead of pnpm

--- react-vite/vite.config.ts
+++ react-vite/vite.config.ts
@@ -6,4 +6,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl(), nodePolyfills()],
+  server: {
+    port: 8080,
+  },
 })
