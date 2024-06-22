a3a451f8cd1af3ddfded790bf0ae72c50a352167
Vihanga Liyanage <vihangaliyanage007@gmail.com>
2024-06-22 16:28:01 -0500

    Add sign in box closing

--- staples.com/src/components/Header.tsx
+++ staples.com/src/components/Header.tsx
@@ -1,3 +1,4 @@
+
 import { FunctionComponent, ReactElement, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
@@ -8,54 +9,64 @@ import {
   SignedIn,
   SignOutButton,
   useAuthentication,
+  useOn,
+  Hooks
 } from "@asgardeo/react";
 import { useState } from 'react';
-import { jwtDecode } from 'jwt-decode';
 
 const Header: FunctionComponent = (): ReactElement => {
 
-  interface DecodedToken {
-    given_name?: string;
-    [key: string]: any;
+  const { user } = useAuthentication();
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+  const [isSignInOverlayVisible, setSignInOverlayVisible] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setSignInOverlayVisible(false);
+      setIsSignedIn(true);
+    }    
+    
+  }, [user]);
+
+  useOn({
+    event: Hooks.SignOut,
+    callback: () => {
+      setIsSignedIn(false);
+    },
+  });
+
+  const handleSignInClick =  () => {
+    toggleOverlay();
   }
 
-  const getGivenNameFromToken = (token: string): string | null => {
-    try {
-      const decoded: DecodedToken = jwtDecode(token);
-      return decoded.given_name || null;
-    } catch (error) {
-      console.error('Failed to decode JWT token:', error);
-      return null;
+  const toggleOverlay = () => {
+    setSignInOverlayVisible(!isSignInOverlayVisible);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    console.log("handleClickOutside");
+    const target = event.target as HTMLElement;
+    if (target.id === 'sign-in-box-container') {
+      toggleOverlay();
     }
   };
 
-  const { user, accessToken } = useAuthentication();
-  const [showSignInBox, setShowSignInBox] = useState<boolean>(false);
-  const [loggedInUserGivenName, setLoggedInUserGivenName] = useState<string|null>(null);
-
   useEffect(() => {
-    if (accessToken) {
-      const givenName = getGivenNameFromToken(accessToken);
-      setLoggedInUserGivenName(givenName);
-      setShowSignInBox(false);
+    if (isSignInOverlayVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
     }
-    
-  }, [accessToken]);
-
-  const handleSignInClick =  () => {
-    const signInButtonStatus = showSignInBox;
-    setShowSignInBox(!signInButtonStatus);
-  }
+  }, [isSignInOverlayVisible]);
 
   return (
     <header>
-      { showSignInBox && 
-        <div className="signInContainer overlay">
+      { isSignInOverlayVisible && 
+        <div className="signInContainer overlay" id='sign-in-box-container'>
           <SignIn 
             showSignUp={true}
             showFooter={false}
-          />
-          
+          />          
         </div>
       }
       <div className="logo">
@@ -66,10 +77,12 @@ const Header: FunctionComponent = (): ReactElement => {
         <input type="text" placeholder="Search..." />
       </div>
       <SignedIn>
-        { loggedInUserGivenName &&
-          <h5 style={{padding: '0px 10px 0px 10px'}}>Welcome, {loggedInUserGivenName}</h5>
+        { isSignedIn &&
+          <>
+            <h5 style={{padding: '0px 10px 0px 10px'}}>Welcome, {user.name.givenName} {user.name.familyName}</h5>
+            <SignOutButton />
+          </>
         }
-        <SignOutButton />
       </SignedIn>
       <div className="header-buttons">
         <button className="header-icon-button" onClick={ () => {handleSignInClick();} }><PersonIcon /></button>
